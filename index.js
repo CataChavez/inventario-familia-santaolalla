20e4fd702c17556b04a18ef19928abbdc9921733
CataChavez <kata.chavez@gmail.com>
2021-06-25 19:11:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const fruits = [
+    {fruitId: 1, name:"Manzana", precio:100, stock:20},
+    {fruitId: 1, name:"Pera", precio:150, stock:10},
+    {fruitId: 1, name:"Naranja", precio:300, stock:10},
+    {fruitId: 1, name:"Plátano", precio:200, stock:5}
+]
+
