051654f6b5a2f2962e89f1c32bea8d472fee3284
CataChavez <kata.chavez@gmail.com>
2021-06-29 16:16:07 -0400

    completed app

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-const fruits = [
-  { fruitId: 1, name: "Manzana", price: 100, stock: 20 },
-  { fruitId: 2, name: "Pera", price: 150, stock: 10 },
-  { fruitId: 3, name: "Naranja", price: 300, stock: 10 },
-  { fruitId: 4, name: "Plátano", price: 200, stock: 5 },
-  { fruitId: 4, name: "Mango", price: 500, stock: 1 },
-];
-//revisar aprendiendo_JS de github
-//Mostrar productos que quedan en stock
 
-//Subir el precio de los productos cuando quede poca existencia
-//(modificar el precio de un producto cuando algún producto tenga una existencia)
-const minStockProducts = () => {
-  const minStock = fruits.map((fruit) => {
-    if(fruit.nombre && fruit.stock !== 1) return fruit
-      return{
-        ...fruit,
-        price: fruit.price * 1.5,
-      }
-    })
-    return minStock
-}
-console.log(minStockProducts())
-
-//Averiguar si pueden vender un producto (si hay suficiente en stock)
-
-//Saber cuanto es el valor total de productos
 
 
